1dc282a0da1d50d8771b41f83e8e4edbc5bfdf3c
Manoa Rasolomalala <manoatony.pro@gmail.com>
2024-04-24 17:08:10 +0200

    Fix bug hunts on Dashboard + Vertical Layout (test hunt 2)

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -1,21 +1,20 @@
 import { formatDate } from '../app/format.js'
-import DashboardFormUI from '../views/DashboardFormUI.js'
 import BigBilledIcon from '../assets/svg/big_billed.js'
 import { ROUTES_PATH } from '../constants/routes.js'
 import USERS_TEST from '../constants/usersTest.js'
+import DashboardFormUI from '../views/DashboardFormUI.js'
+
 import Logout from "./Logout.js"
 
 export const filteredBills = (data, status) => {
-  return (data && data.length) ?
-    data.filter(bill => {
+  return (data && data.length)
+    ? data.filter((bill) => {
       let selectCondition
 
       // in jest environment
       if (typeof jest !== 'undefined') {
         selectCondition = (bill.status === status)
-      }
-      /* istanbul ignore next */
-      else {
+      } else { /* istanbul ignore next */
         // in prod environment
         const userEmail = JSON.parse(localStorage.getItem("user")).email
         selectCondition =
@@ -24,15 +23,14 @@ export const filteredBills = (data, status) => {
       }
 
       return selectCondition
-    }) : []
+    })
+    : []
 }
 
 export const card = (bill) => {
   const firstAndLastNames = bill.email.split('@')[0]
-  const firstName = firstAndLastNames.includes('.') ?
-    firstAndLastNames.split('.')[0] : ''
-  const lastName = firstAndLastNames.includes('.') ?
-  firstAndLastNames.split('.')[1] : firstAndLastNames
+  const firstName = firstAndLastNames.includes('.') ? firstAndLastNames.split('.')[0] : ''
+  const lastName = firstAndLastNames.includes('.') ? firstAndLastNames.split('.')[1] : firstAndLastNames
 
   return (`
     <div class='bill-card' id='open-bill${bill.id}' data-testid='open-bill${bill.id}'>
@@ -40,10 +38,12 @@ export const card = (bill) => {
         <div class='bill-card-name'> ${firstName} ${lastName} </div>
         <span class='bill-card-grey'> ... </span>
       </div>
+
       <div class='name-price-container'>
         <span> ${bill.name} </span>
         <span> ${bill.amount} € </span>
       </div>
+
       <div class='date-type-container'>
         <span> ${formatDate(bill.date)} </span>
         <span> ${bill.type} </span>
@@ -53,7 +53,7 @@ export const card = (bill) => {
 }
 
 export const cards = (bills) => {
-  return bills && bills.length ? bills.map(bill => card(bill)).join("") : ""
+  return bills && bills.length ? bills.map((bill) => card(bill)).join("") : ""
 }
 
 export const getStatus = (index) => {
@@ -72,16 +72,20 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
+
     new Logout({ localStorage, onNavigate })
   }
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
+
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} alt="Bill"/></div>`)
+
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
@@ -89,7 +93,7 @@ export default class {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
-      bills.forEach(b => {
+      bills.forEach((b) => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
@@ -105,6 +109,7 @@ export default class {
       $('.vertical-navbar').css({ height: '120vh' })
       this.counter ++
     }
+
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -116,6 +121,7 @@ export default class {
       status: 'accepted',
       commentAdmin: $('#commentary2').val()
     }
+
     this.updateBill(newBill)
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
@@ -126,51 +132,51 @@ export default class {
       status: 'refused',
       commentAdmin: $('#commentary2').val()
     }
+
     this.updateBill(newBill)
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
   handleShowTickets(e, bills, index) {
+    let filterBills
+
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
+
+    filterBills = filteredBills(bills, getStatus(this.index))
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
+      $(`#status-bills-container${this.index}`).html(cards(filterBills))
       this.counter ++
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
+      $(`#status-bills-container${this.index}`).html("")
       this.counter ++
     }
 
-    bills.forEach(bill => {
+    /* ** Bug fix : change the array from 'bills' to 'filterBills' ** */
+    filterBills.forEach((bill) => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
-
   }
 
   getBillsAllUsers = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-        .map(doc => ({
-          id: doc.id,
-          ...doc,
-          date: doc.date,
-          status: doc.status
-        }))
-        return bills
-      })
-      .catch(error => {
-        throw error;
-      })
+        .bills()
+        .list()
+        .then((snapshot) => {
+          const bills = snapshot.map((doc) => ({
+            id: doc.id,
+            ...doc,
+            date: doc.date,
+            status: doc.status
+          }))
+          return bills
+        })
+        .catch(error => { throw error })
     }
   }
 
@@ -181,7 +187,7 @@ export default class {
     return this.store
       .bills()
       .update({data: JSON.stringify(bill), selector: bill.id})
-      .then(bill => bill)
+      .then((bill) => bill)
       .catch(console.log)
     }
   }
